6cf8cf3e0c3011f5cccd55a5fa4f0e982eab73d3
RATelectro <rom.ted@mail.ru>
2018-11-04 12:11:14 +0300

    added sequencer

--- WinProj/Src/sequencer.cpp
+++ WinProj/Src/sequencer.cpp
@@ -7,34 +7,44 @@
 
 #include <Sequencer.hpp>
 
-Sequencer::Sequencer() : prevTime(0), currStep(0), seqLength(SeqOptions::SIZE) { };
+Sequencer::Sequencer() : seqLength(SeqOptions::SIZE), currStep(0), prevTime(0) { };
 
-void triggerStep(SeqStep step) {
-	SendNoteOn( &(step.note) );
-	HAL_Delay(step.length);       // ?? stupid
-	SendNoteOff( &(step.note) );  // noteOff with same velocity
-	HAL_Delay(step.length);       // stupid monophonic
+void triggerStep(SeqStep step) {  // TODO: think about timing
+	if (step.repeats)
+		for(u8 i = 0; i < step.repeats; i++) {
+			SendNoteOn( &(step.note) );
+			HAL_Delay(step.length);       // ?? stupid
+			SendNoteOff( &(step.note) );  // noteOff with same velocity
+			HAL_Delay(step.length);       // stupid monophonic
+		}
+	else
+		SendNoteOn( &(step.note) );
+		HAL_Delay(step.length);       // ?? stupid
+		SendNoteOff( &(step.note) );  // noteOff with same velocity
+		HAL_Delay(step.length);       // stupid monophonic
 }
 
 void Sequencer::advanceStep(void) {
-	if(sequence[currStep].jumpTo != NULL)  // jump to hidden steps? Fun)
-		currStep = sequence[currStep].jumpTo;
+	if(sequence[currStep].action & JUMP)     // if current step is set to JUMP - jump to the step specified
+		currStep = sequence[currStep].jumpTo;   // jump to hidden steps? Fun)
 	else
-		currStep++;
+		currStep++;  // else simply advance step
 
-	if(currStep >= seqLength)
+	while ( (sequence[currStep].action == SKIP) && (currStep < seqLength))  // if the next step is SKIP - advannce more
+		 currStep++;                                                        // until the next step is not SKIP
+
+	if(currStep >= seqLength) {  // cycle steps
 		currStep = 0;
+		while ( (sequence[currStep].action == SKIP) && (currStep < seqLength))  // SKIP check again
+				 currStep++;
 
-	if (sequence[currStep].repeats)
-		for(u8 i = 0; i < sequence[currStep].repeats; i++) {
-			triggerStep(sequence[currStep]);
-		}
-	else
-		triggerStep(sequence[currStep]);
+	}
+
 }
 
 void Sequencer::start(void) {
-	triggerStep(sequence[currStep]);
-	while(1)         // ?? stuupid
+	while(1) {         // ?? stuupid
 		Sequencer::advanceStep();
+		triggerStep(sequence[currStep]);
+	}
 }
