c800d4734de54282cd856195e3cd3e13a69ba3f4
RATelectro <rom.ted@mail.ru>
2018-11-04 12:30:32 +0300

    added sequencer

--- WinProj/Src/sequencer.cpp
+++ WinProj/Src/sequencer.cpp
@@ -9,19 +9,23 @@
 
 Sequencer::Sequencer() : seqLength(SeqOptions::SIZE), currStep(0), prevTime(0) { };
 
-void triggerStep(SeqStep step) {  // TODO: think about timing
-	if (step.repeats)   // ?? same behavior for repeats = 0 and repeats = 1
-		for(u8 i = 0; i < step.repeats; i++) {
-			SendNoteOn( &(step.note) );
-			HAL_Delay(step.length);       // ?? stupid
-			SendNoteOff( &(step.note) );  // noteOff with same velocity
-			HAL_Delay(step.length);       // stupid monophonic
+void triggerStep(SeqStep step) {  // TODO: think about timing, initialise all steps in ktor
+	// if (step.action & REPEAT)
+		for (u8 i = 0; i < step.repeats; i++) { // ?? behavior for repeats = 0 (ktor)
+			if (step.action & MUTE)               // if muted - no sound, just wait
+				HAL_Delay(step.length * 2);       // ?? stupid
+			else {                                // else play
+				SendNoteOn(step.note.key, step.note.velocity);
+				HAL_Delay(step.length);                          // ?? stupid
+				SendNoteOff(step.note.key, step.note.velocity);  // noteOff with same velocity
+				HAL_Delay(step.length);                          // stupid monophonic
+			}
 		}
-	else
-		SendNoteOn( &(step.note) );
-		HAL_Delay(step.length);       // ?? stupid
-		SendNoteOff( &(step.note) );  // noteOff with same velocity
-		HAL_Delay(step.length);       // stupid monophonic
+//	else                           // or play once ()
+//		SendNoteOn( &(step.note) );
+//		HAL_Delay(step.length);       // ?? stupid
+//		SendNoteOff( &(step.note) );  // noteOff with same velocity
+//		HAL_Delay(step.length);       // stupid monophonic
 }
 
 void Sequencer::advanceStep(void) {
@@ -30,12 +34,12 @@ void Sequencer::advanceStep(void) {
 	else
 		currStep++;  // else simply advance step
 
-	while ( (sequence[currStep].action == SKIP) && (currStep < seqLength))  // if the next step is SKIP - advannce more
+	while ( (sequence[currStep].action & SKIP) && (currStep < seqLength))  // if the next step is SKIP - advannce more
 		 currStep++;                                                        // until the next step is not SKIP
 
 	if(currStep >= seqLength) {  // cycle steps
 		currStep = 0;
-		while ( (sequence[currStep].action == SKIP) && (currStep < seqLength))  // SKIP check again
+		while ( (sequence[currStep].action & SKIP) && (currStep < seqLength))  // SKIP check again
 				 currStep++;
 
 	}
